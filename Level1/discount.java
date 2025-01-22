47c6d791a5d248f763d3e47fbac2c558c540fd6d
Lakshay0205 <140381171+Lakshay0205@users.noreply.github.com>
2025-01-22 22:29:32 +0530

    Delete Level1 directory

--- Level1/discount.java
+++
@@ -1,9 +0,0 @@
-public class discount{
-    public static void main(String[] args) {
-        int fee = 125000;
-        int discountPercent = 10;
-        int discount = (fee * discountPercent) / 100;
-        int discountedFee = fee - discount;
-        System.out.println("Discounted Fee: " + discountedFee);
-        }
-}
